bc41980ac56b661dcc6970780a8f0ebbd4fad22a
d-tomerini <daniele.tomerini@epfl.ch>
2019-11-12 09:59:38 +0100

    WIP

--- aiida_post/workflows/bandgap.py
+++ aiida_post/workflows/bandgap.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from aiida import orm
 from aiida.engine import WorkChain, ToContext
 from aiida_quantumespresso.workflows.pw.band_structure import PwBandStructureWorkChain
-from aiida.orm.nodes.data.array.bands import find_bandgap
 
 
 class PwBandGapWorkChain(WorkChain):
@@ -19,14 +18,16 @@ class PwBandGapWorkChain(WorkChain):
         spec.input('code', valid_type=orm.Code)
         spec.input('structure', valid_type=orm.StructureData)
         spec.outline(cls.run_band_structure, cls.get_bandgap)
-        spec.output('band_gap', valid_type=orm.Float)
+        spec.output('output_parameters', valid_type=orm.Dict)
 
     def run_band_structure(self):
         """
         Run the PwBandsWorkChain to compute the band structure
         """
 
-        inputs = {'structure': self.inputs.structure, 'code': self.inputs.code}
+        inputs = {
+                'structure': self.inputs.structure,
+                'code': self.inputs.code}
 
         running = self.submit(PwBandStructureWorkChain, **inputs)
         self.report('launching PwBandStructureWorkChain<{}>'.format(running.pk))
@@ -37,10 +38,13 @@ class PwBandGapWorkChain(WorkChain):
         From the band object obtained by the band structure
         calculation, run a simple script to obtain the band gap
         """
+        from aiida.orm.nodes.data.array.bands import find_bandgap
 
         bands = self.ctx.workchain_bands.outputs.band_structure
         is_insulator, bandgap = find_bandgap(bands)
-        self.ctx.band_gap = orm.Float(bandgap)
-        self.out('band_gap', self.ctx.band_gap)
+        output = {}
+        output['band_gap'] = bandgap
+        output['is_insulator'] = is_insulator
+        self.out('output_parameters', Dict(dict=output))
         self.report('calculation completed')
         return
